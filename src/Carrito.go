97b3a4d9df3ffa88818a777ea95761bd51bcf47d
victoria <victoriabladilo@gmail.com>
2026-03-24 19:19:45 -0300

    termine la clase carrito para almacenar items de compra

--- src/Carrito.go
+++ src/Carrito.go
@@ -8,4 +8,10 @@ func (c *Carrito) AddItem(item ItemCompra) { //modifica el carrito original con 
 	c.Items = append(c.Items, item)
 }
 
-func (c *Carrito) Total()
+func (c *Carrito) Total() float64 {
+	total := 0.0
+	for _, item := range c.Items {
+		total += item.PrecioPorItem()
+	}
+	return total
+}
