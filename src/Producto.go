be8d2c0dc7f90dd86cb9026b497cf19a65225ec7
victoria <victoriabladilo@gmail.com>
2026-04-02 14:23:14 -0300

    Cambie una clase producto para volverla interface

--- src/Producto.go
+++ src/Producto.go
@@ -1,7 +1,22 @@
 package main
 
-type Producto struct {
+type Producto interface {
+	ID int
 	Nombre string
 	Precio float64
+	Stock int
+	Categoria string
 }
+
+/*func (p Producto) HayStock(cantidad int) bool{
+	return p.Stock >= cantidad
+}
+
+func (p *Producto) ReducirStock(cantidad int) {
+	if p.HayStock(cantidad) {
+		p.Stock -= cantidad
+		return true
+	}
+	return false
+}/*
 
