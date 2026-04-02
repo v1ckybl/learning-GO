4b5c2cf87dcc90b524ba85a470cd37e68983060a
victoria <victoriabladilo@gmail.com>
2026-04-02 17:07:26 -0300

    primer clase hecha

--- src/Electronico.go
+++ src/Electronico.go
@@ -1,7 +1,24 @@
 package main
 
+import (
+	"fmt"
+)
+
 type Electronico struct {
 	ProductoBase
 	marca    string
 	garantia int // meses
 }
+
+func NewElectronico(nombre string, precio float64, stock int, marca string, garantia int) *Electronico {
+	return &Electronico{
+		ProductoBase:  nuevoProductoBase(nombre, precio, stock),
+		marca:    marca,
+		garantia: garantia,
+	}
+}
+
+func (e *Electronico) Descripcion() string {
+	return fmt.Sprintf("[Electrónico] #%d | %s | Marca: %s | Garantía: %d meses | $%.2f",
+		e.id, e.nombre, e.marca, e.garantia, e.precio)
+} //dudoso a ver che
