8048dc05959c9e16da49b46e60f2e5e2bc60f7f7
victoria <victoriabladilo@gmail.com>
2026-04-05 00:21:18 -0300

    test de como se luberaria el stock

--- Go-Todo/src/Productou_test.go
+++ Go-Todo/src/Productou_test.go
@@ -22,6 +22,24 @@ func TestIDsUnicosPorProducto(t *testing.T) {
 }
 
 func TestStock(t *testing.T) {
-	g:= nuevoGen()
-	p:= NewLibro("Las cronicas de narnia", 1000.05, 7, "C.S. Lewis", "Fantasía")
-	if 
+	p := NewLibro("Las cronicas de narnia", 1000.05, 7, "C.S. Lewis", "Fantasía")
+	_ = p.Reservar(2)
+	// El stock original no deberia cambiar
+	if p.GetStock() != 7 {
+		t.Errorf("El stock original no deberia cambiar aun y se obtuvo %d", p.GetStockDisponible())
+	}
+	// El stock disponible si deberia cambiar
+	if p.GetStockDisponible() != 5 {
+		t.Errorf("stock disponible debería ser 5, got %d", p.GetStockDisponible())
+	}
+}
+
+func TestLiberarStock(t *testing.T) {
+	p := NewRopa("Camisa", 25.00, 10, "M", "Algodón")
+	_ = p.Reservar(3)
+	p.LiberarReserva(2)
+
+	if p.GetStockDisponible() != 9 {
+		t.Errorf("stock disponible debería ser 9 después de liberar reserva, got %d", p.GetStockDisponible())
+	}
+}
